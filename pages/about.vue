4a756f3c64ae464f5807fa328ecf3e72a6653876
Figo Arbiansyah <100900066+FigoArbiansyah@users.noreply.github.com>
2023-05-17 19:45:47 +0700

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,6 @@
 <template>
   <Nav/>
-  <div class="px-20 min-h-screen flex items-center justify-center">
+  <div class="md:px-20 px-5 min-h-screen flex items-center justify-center">
     <div class="text-center">
       <p class="text-3xl font-bold">Tentang Blogo!</p>
       <div class="mt-8">
