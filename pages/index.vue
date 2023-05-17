6a0d6d358379255d3b76d41af1c21c117fc6db38
Figo Arbiansyah <100900066+FigoArbiansyah@users.noreply.github.com>
2023-05-17 19:45:13 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <Nav/>
-  <div class="px-20 min-h-screen flex items-center justify-center">
+  <div class="md:px-20 px-5 min-h-screen flex items-center justify-center">
     <div class="text-center">
       <p class="text-3xl font-bold">Selamat Datang di Blogo!</p>
       <div class="mt-8">
